d3a41602143af91f7fc1d34dbbf1021cebbf1a08
Dylan McDonald <dylan@dlmconsulting.org>
2021-03-29 10:23:49 -0400

    Initial upload

---
+++ Core Data Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Core Data Test
+//
+//  Created by Dylan McDonald on 3/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Core Data TestTests/Core_Data_TestTests.swift
@@ -0,0 +1,33 @@
+//
+//  Core_Data_TestTests.swift
+//  Core Data TestTests
+//
+//  Created by Dylan McDonald on 3/28/21.
+//
+
+import XCTest
+@testable import Core_Data_Test
+
+class Core_Data_TestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
